602e96053cc3813666cf8663ee571be935cbfb55
Puslore <mini.big10@mail.ru>
2024-10-14 19:33:32 +0800

    pr_5 done 14.10.2024

--- pr_5.py
+++ pr_5.py
@@ -1,24 +1,23 @@
 # Task 1 var 3
-from unicodedata import digit
 
-# first_str = input('First string: ')
-# second_str = input('Second string: ')
-#
-# def shifting(string_a: str, string_b: str):
-#     shift_b = [string_b[i:] + string_b[:i] for i in range(len(string_b))]
-#     cnt = 0
-#     for shift in shift_b:
-#         pos = 0
-#         while pos != -1:
-#             pos = string_a.find(shift, pos)
-#             if pos != -1:
-#                 cnt += 1
-#                 pos += len(shift)
-#
-#     return cnt
-#
-#
-# print(shifting(first_str, second_str))7
+first_str = input('First string: ')
+second_str = input('Second string: ')
+
+def shifting(string_a: str, string_b: str):
+    shift_b = [string_b[i:] + string_b[:i] for i in range(len(string_b))]
+    cnt = 0
+    for shift in shift_b:
+        pos = 0
+        while pos != -1:
+            pos = string_a.find(shift, pos)
+            if pos != -1:
+                cnt += 1
+                pos += len(shift)
+
+    return cnt
+
+
+print(shifting(first_str, second_str))
 # ----------------------------------------------------------------------------------------
 # Task 2
 
