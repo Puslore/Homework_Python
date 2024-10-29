2ae07dd4d1c90678718cd2e199dfcf7dae2c7749
Puslore <mini.big10@mail.ru>
2024-10-29 14:32:25 +0800

    unstable 29.10.2024

--- pr_7.py
+++ pr_7.py
@@ -2,34 +2,54 @@
 
 import re
 
-from Cython.Compiler.Parsing import print_parse_tree
 
 with open('roles.txt', 'r') as file:
     roles = [f'{file.readline()[:-1]}:' for _ in range(26)]
     roles.pop(0)
     db = {_: [] for _ in roles}
     reg = r'^[А-ЯЁа-яё ]*:'
-    print(db)
+    # print(db)
     file.readline()
     cnt = 1
-
-    #         db[name].append(f'{cnt}) {line.strip(name)}')
 
     while True:
         line = file.readline().strip('\n')
         if line == '': break
         name = ''.join(re.findall(reg, line))
-        phrase = ''
+        phrase = line
 
-        if name in line:
-            phrase += f'{cnt}) {line.strip(name)}'
-            continue
+        if name != '' and name in roles:
+            # db[name].append(f'{cnt}) {line.strip(name)}')
+            pers_name = name
+            phrase = line
 
-        elif name not in line:
-            phrase += line
+        elif name == '':
+            while ''.join(re.findall(reg, line)) == '':
+                phrase += line
+                line = file.readline().strip('\n')
 
+        db[pers_name].append(f'{cnt}) {phrase.strip(pers_name)}')
         cnt += 1
 
+
+
+        # print(phrase)
+        # print(db)
+
+        # if name != '':
+        #     phrase = f'{cnt}) {line.strip(name)}'
+        #     # line = file.readline().strip('\n')
+        #
+        #     # if ''.join(re.findall(reg, line)) != '':
+        #     #     db[name].append(f'{cnt}) {line.strip(name)}')
+        #     #     break
+        #
+        #     while ''.join(re.findall(reg, line)) == '':
+        #         line = file.readline().strip('\n')
+        #         phrase += line
+
+        # print(phrase)
+        # db[name].append(f'{cnt}) {phrase.strip(name)}')
 
 for i in db.items():
     print(i[0])
