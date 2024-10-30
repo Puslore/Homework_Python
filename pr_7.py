9ee540d5f1bf365774143299823f749deb3e8107
Puslore <mini.big10@mail.ru>
2024-10-30 23:28:57 +0800

    remaking pr_7 30.10.2024

--- pr_7.py
+++ pr_7.py
@@ -3,12 +3,12 @@
 import re
 
 
-with open('roles.txt', 'r') as file:
+with open('roles.txt', 'r', encoding='utf-8') as file:
     roles = [f'{file.readline()[:-1]}:' for _ in range(26)]
     roles.pop(0)
     db = {_: [] for _ in roles}
     reg = r'^[А-ЯЁа-яё ]*:'
-    # print(db)
     file.readline()
-    cnt = 0
-    buffer_line = ''
+    all_lines = [''.join(x).strip('\n') for x in file.readlines()]
+
+
