7715223a4f903f7c170adbf29afca96bf37d13b5
Puslore <mini.big10@mail.ru>
2024-10-31 23:12:58 +0800

    pr_7 done 31.10.2024

--- pr_7.py
+++ pr_7.py
@@ -4,12 +4,13 @@ import re
 
 
 def make_buffer_list(number, role, phrase):
-    mbl = [f'{number}{role}', phrase.strip(role)]
+    mbl = [number, role, phrase.strip(role)]
     return mbl
 
 with open('roles.txt', 'r', encoding='utf-8') as file:
     # creating roles list
-    roles = [f'{file.readline()}:' for _ in range(26)]
+    a = "\n"
+    roles = [f'{file.readline().strip(a)}:' for _ in range(26)]
     roles.pop(0)
     # db - dict with roles-keys and phrases-values
     db = {_: [] for _ in roles}
@@ -25,16 +26,25 @@ with open('roles.txt', 'r', encoding='utf-8') as file:
 
     # trying to solve task with for-cycle
     for phrase in all_lines:
-        role = re.findall(reg, phrase)
-        if role != '':
+        role = ''.join(re.findall(reg, phrase))
+
+        if role != '' and role in roles:
             cnt += 1
             buffer_list.append(make_buffer_list(cnt, role, phrase))
             key = cnt - 1
             buffer_phrase = phrase
 
         elif role == '':
-            buffer_list[key][1].append(phrase)
+            buffer_list[key][2] += phrase
             buffer_phrase = ''
 
 
-print(buffer_list)
+for actors in buffer_list:
+    db[actors[1]].append(f'{actors[0]}) {actors[2]}')
+
+
+with open('output.txt', 'w') as file:
+    for i in db.items():
+        file.write(f'{i[0]}\n')
+        for j in i[1]:
+            file.write(f'{j}\n')
