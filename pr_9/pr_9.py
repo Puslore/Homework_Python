80cd9ee760c98e0083f79044659a1afa2685fde8
Puslore <mini.big10@mail.ru>
2024-11-02 18:25:02 +0800

    breakpoint 02.11.2024

---
+++ pr_9/pr_9.py
@@ -0,0 +1,42 @@
+# Task 9 - OpenWeather
+
+import requests
+import telebot
+
+
+BOT_TOKEN = '5639979585:AAF-IDS7WoVAZ2jm-aLoUs4ZbazHMWERy9A'
+WEATHER_TOKEN = '299147b63c1c74d4df3eaea5fd7cfc3a'
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def get_weather(city):
+    try:
+        req = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_TOKEN}&units=metric")
+        data = req.json()
+        weather = {
+            'temp': data["main"]["temp"],
+            'pressure': data["main"]["pressure"],
+            'wind_speed': data["wind"]["speed"]
+        }
+        weather_unpack = dict_unpack(weather, city)
+        return weather_unpack
+    except:
+        err_msg = 'Проверьте название города - возможно вы ошиблись'
+        return err_msg
+
+def dict_unpack(dict_, city):
+    string_ = (f'Температура в городе {city}: {dict_["temp"]}° по Цельсию\n'
+               f'Давление: {dict_["pressure"]} HPA\nСкорость ветра: {dict_["wind_speed"]} метра в секунду')
+    return string_
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, f'Привет, {message.chat.first_name}! Напиши название города, в котором хочешь узнать погоду')
+
+@bot.message_handler(content_types=['text'])
+def send_weather(message):
+    weather = get_weather(message.text)
+    bot.send_message(message.chat.id, weather)
+
+bot.infinity_polling()
