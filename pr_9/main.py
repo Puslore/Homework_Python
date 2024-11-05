fbd52769a8891f7470d599075ea48e73ca72373e
Puslore <mini.big10@mail.ru>
2024-11-05 21:01:44 +0800

    pr_9 done 05.11.2024

--- pr_9/main.py
+++ pr_9/main.py
@@ -2,41 +2,62 @@
 
 import requests
 import telebot
+import os
+from datetime import datetime
+import pytz
 
 
-BOT_TOKEN = '5639979585:AAF-IDS7WoVAZ2jm-aLoUs4ZbazHMWERy9A'
-WEATHER_TOKEN = '299147b63c1c74d4df3eaea5fd7cfc3a'
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_TOKEN = os.getenv("WEATHER_TOKEN")
+
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-def get_weather(city):
+def get_weather(city, user):
     try:
-        req = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_TOKEN}&units=metric")
+        req = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_TOKEN}&units=metric')
         data = req.json()
-        weather = {
-            'temp': data["main"]["temp"],
-            'pressure': data["main"]["pressure"],
-            'wind_speed': data["wind"]["speed"]
-        }
-        weather_unpack = dict_unpack(weather, city)
-        return weather_unpack
-    except:
-        err_msg = 'Проверьте название города - возможно вы ошиблись'
-        return err_msg
+
+        if req.status_code == 200 and "main" in data and "wind" in data:
+            weather = {
+                'temp': data["main"]["temp"],
+                'pressure': data["main"]["pressure"],
+                'wind_speed': data["wind"]["speed"]
+            }
+            return dict_unpack(weather, city)
+
+        else:
+            return 'Проверьте название города.'
+
+    except Exception as err:
+        logging(user, f'ERROR: {err}')
+        return 'Проверьте название города - возможно вы ошиблись. Возможна ошибка со стороны сервиса'
+
 
 def dict_unpack(dict_, city):
-    string_ = (f'Температура в городе {city}: {dict_["temp"]}° по Цельсию\n'
-               f'Давление: {dict_["pressure"]} HPA\nСкорость ветра: {dict_["wind_speed"]} метра в секунду')
-    return string_
+    return (f'Температура в городе {city}: {dict_["temp"]}°C\n'
+            f'Давление: {dict_["pressure"]} HPa\n'
+            f'Скорость ветра: {dict_["wind_speed"]} м/с')
+
+
+def logging(user, message):
+    time = datetime.now(tz=pytz.timezone('Asia/Irkutsk')).date()
+    with open('./logs.txt', 'a') as file:
+        file.write(f'{user} : {message}  {time}\n')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, f'Привет, {message.chat.first_name}! Напиши название города, в котором хочешь узнать погоду')
+    bot.send_message(message.chat.id,f'Привет, {message.chat.first_name}! Напиши название города, в котором хочешь узнать погоду.')
+
 
 @bot.message_handler(content_types=['text'])
 def send_weather(message):
-    weather = get_weather(message.text)
+    city = message.text.strip()
+    user = message.chat.first_name
+    weather = get_weather(city, user)
     bot.send_message(message.chat.id, weather)
+    logging(user, city)
+
 
 bot.infinity_polling()
