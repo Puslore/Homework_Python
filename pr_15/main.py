b9e24f884fc0aa01ef5cb1cb86d110f6fe9fccb3
Puslore <mini.big10@mail.ru>
2024-12-10 23:27:24 +0800

    checkpoint

--- pr_15/main.py
+++ pr_15/main.py
@@ -5,34 +5,35 @@ import pytz
 
 class BirthInfo:
     @singledispatchmethod
-    def __init__(self, date):
+    def __init__(self):
         raise TypeError('Аргумент переданного типа не поддерживается')
 
     @__init__.register(datetime)
     def __init__date_class(self, date):
         self.birth_time = date
-        print(self.birth_time)
 
     @__init__.register(str)
     def __init__iso(self, date):
-        self.birth_time = datetime(*date.split())
-        print(self.birth_time)
+        try:
+            self.birth_time = datetime.strptime(date, '%d-%m-%Y')
+
+        except ValueError:
+            raise ValueError('incorrect str')
 
     @__init__.register(list)
     def __init__list(self, date):
-        self.birth_time = datetime(*date.split())
-        print(self.birth_time)
+        try:
+            self.birth_time = datetime(*date.split())
 
     @__init__.register(set)
     def __init__list(self, date):
         self.birth_time = datetime(*date.split())
-        print(self.birth_time)
 
     def age(self):
         return (datetime.now(tz=pytz.timezone('Asia/Irkutsk')).date() - self.birth_time.date()) / 365
 
 
-print(BirthInfo(datetime(2023, 09, 22)).age())
+print(BirthInfo(datetime(2000, 9, 22)).age())
 # can work incorrect. I don't know how to fix that. There's troubles with datetime module
 
 # a = datetime.now(tz=pytz.timezone('Asia/Irkutsk')).date()
