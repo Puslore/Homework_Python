45bb5b5925c18019163b2eb02ff223beddf18e09
Puslore <mini.big10@mail.ru>
2024-12-10 22:40:42 +0800

    pr_15_sesond started 10.12.2024

--- pr_15/main.py
+++ pr_15/main.py
@@ -1,39 +1,8 @@
-# Task 1
 from datetime import datetime
 from functools import singledispatchmethod
 import pytz
 
 
-class Negator:
-    @singledispatchmethod
-    def neg(inpt):
-        raise TypeError('Аргумент переданного типа не поддерживается')
-
-    @neg.register(int)
-    def neg_int(inpt):
-        return -inpt
-
-    @neg.register(float)
-    def neg_float(inpt):
-        return -inpt
-
-    @neg.register(bool)
-    def neg_bool(inpt):
-        return not inpt
-
-
-print(Negator.neg(True))
-print(Negator.neg(False))
-print(Negator.neg(2))
-print(Negator.neg(-10))
-print(Negator.neg(24.8))
-print(Negator.neg(-3.14))
-# print(Negator.neg('stop!'))
-
-
-
-# ---------------------------------------------------------------------------------------
-# Task 2
 class BirthInfo:
     @singledispatchmethod
     def __init__(self, date):
